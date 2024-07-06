ee857167031f68baab478826bac70e58603c7242
bnh5992 <95531379+bnh5992@users.noreply.github.com>
2024-07-06 16:49:19 +0900

    Update SfuStream.js

--- src/Media/SfuStream.js
+++ src/Media/SfuStream.js
@@ -223,7 +223,7 @@ const SfuStream = () => {
         useEffect(() => {
                 console.log("호출")
             if(isEnter){
-                ws.current = new WebSocket('wss://0.tcp.jp.ngrok.io:16699/signal');
+                ws.current = new WebSocket('ws://0.tcp.jp.ngrok.io:16699/signal');
                 ws.current.onopen = () => {
                     register();
                 };
